7b0a1aee82495be0b1dc0a59118bbb3e999b0053
MrRecordHolder <pokecloudbot@gmail.com>
2020-05-15 11:38:11 -0400

    server settings default values

---
+++ keys/guildSettings.js
@@ -0,0 +1,28 @@
+const guildSettings = {
+    server: {
+        name: "",
+        id: "",
+        language: "English",
+        prefix: "$",
+    },
+    location: {
+        city: "",
+        state: "",
+        country: "USA",
+    },
+    channels: {
+        admin: "",
+        log: ""
+    },
+    roles: {
+        admin: "",
+        reporter: ""
+    },
+    migration: {
+        tag: false,
+        tagrole: "",
+        tagchannel: ""
+    },
+}
+
+module.exports = guildSettings
