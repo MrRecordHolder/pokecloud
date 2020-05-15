a7fdcda66f11fced5577f686d213af04b004ba78
MrRecordHolder <pokecloudbot@gmail.com>
2020-05-15 11:38:26 -0400

    pokedex default values

---
+++ keys/goPokedex.js
@@ -0,0 +1,29 @@
+const goPokedex = {
+    name: {
+        english: "",
+        german: "",
+        nickname: [""]
+    },
+    dex: "",
+    type: {
+        primary: "",
+        secondary: ""
+    },
+    boost: {
+        primary: "",
+        secondary: ""
+    },
+    shiny: {
+        general: false,
+        wild: false,
+        evolution: false,
+        alolan: false
+    },
+    form: {
+        alolan: false,
+        shadow: false
+    },
+    nest: false
+}
+
+module.exports = goPokedex
