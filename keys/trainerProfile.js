e1c8fcb89c6d2fbab23f2cdc3ec860da0063096d
MrRecordHolder <pokecloudbot@gmail.com>
2020-07-31 19:37:52 -0400

    creation points

--- keys/trainerProfile.js
+++ keys/trainerProfile.js
@@ -8,7 +8,7 @@ const trainerProfile = {
     },
     stats: {
         nest_reports: 0,
-        total_points: 0,
+        total_points: 50, // x2 for a limited time
         level: 1
     },
     patreon: false,
