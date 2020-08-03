ed49221032697eeee8d9e1402cf18e1104a9f627
MrRecordHolder <pokecloudbot@gmail.com>
2020-08-03 15:38:14 -0400

    Update trainerProfile.js

--- keys/trainerProfile.js
+++ keys/trainerProfile.js
@@ -1,13 +1,18 @@
 const trainerProfile = {
     discord: {
-        id: ""
+        id: "",
+        serverid: ""
     },
     trainer: {
         name: "",
-        code: ""
+        code: "",
+        level: 1,
+        xp: 0
     },
     stats: {
         nest_reports: 0,
+        nest_created: 0,
+        nest_updated: 0,
         total_points: 50, // x2 for a limited time
         level: 1
     },
